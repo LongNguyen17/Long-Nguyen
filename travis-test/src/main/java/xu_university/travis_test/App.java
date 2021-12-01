b259b794994ca1788631dc6a1560552f3c157429
LongNguyen17 <l.nguyen@student.xu-university.de>
2021-12-01 14:19:47 +0100

    Maven Project Created

---
+++ travis-test/src/main/java/xu_university/travis_test/App.java
@@ -0,0 +1,13 @@
+package xu_university.travis_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
